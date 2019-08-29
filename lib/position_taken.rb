83fd2dc44847902cbfa5a69744162b48099349f8
Vik Vangala <vvang044@gmail.com>
2019-08-29 12:38:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   taken = nil
-  if ( board[index] == '' )
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
     taken = false
 end
